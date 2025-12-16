a8c92e0e7e58076346be098f699b32be8ae123d6
anas1846 <muhammadanas1846@gmail.com>
2025-12-16 11:24:03 +0500

    change

--- src/Top Recipes.jsx
+++ src/Top Recipes.jsx
@@ -8,7 +8,21 @@ import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 
 const TopRecipes = ({ search, onSelect,select}) => {
- const [liked, setLiked] = useState(false);
+ const [likedRecipes, setLikedRecipes] = useState(() => {
+  return JSON.parse(localStorage.getItem("likedRecipes")) || [];
+});
+ 
+
+const toggleLike = (id) => {
+  setLikedRecipes((prev) => {
+    const updated = prev.includes(id)
+      ? prev.filter((item) => item !== id)
+      : [...prev, id];
+
+    localStorage.setItem("likedRecipes", JSON.stringify(updated));
+    return updated;
+  });
+};
 
   const filtered = recipes.filter(item =>
     item.name.toLowerCase().includes(search.toLowerCase())
@@ -26,7 +40,21 @@ const TopRecipes = ({ search, onSelect,select}) => {
               <div className="card-content">
                 <div className="checkbox">
                 <h3>{item.name}</h3>
-                  <input type="checkbox" onChange={()=>onSelect(item)} checked={select.includes(item)} />
+                 <FontAwesomeIcon icon={likedRecipes  ? solidHeart : regularHeart} 
+                onClick={() => {
+  toggleLike(item.id);
+  onSelect(item);
+}}
+                 style={{
+                 color: likedRecipes.includes(item.id) ? "red" : "#888",
+        cursor: "pointer",
+        fontSize: "22px",
+        transition: "0.3s",
+                 }}
+                //  onChange={()=>onSelect(item)}
+                 checked={select.includes(item)}
+                 
+                 />
 
               </div>
               <p><NavLink className="detail" to={`/rescipe-detail/${i}`}>View Recipe</NavLink></p>
@@ -34,7 +62,7 @@ const TopRecipes = ({ search, onSelect,select}) => {
             </div>
           ))
         )}
-      </div>
+      </div> 
     </div>
   );
 }
